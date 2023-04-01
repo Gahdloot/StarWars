41c0d2e7e5228734eb2c84e0745c931de9d33319
Michael David <davidlearn27@gmail.com>
2023-04-01 08:42:55 +0100

    added episode id

--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 class Film(models.Model):
     title = models.CharField(max_length=255)
+    episode_id = models.IntegerField()
     comment_count = models.IntegerField(default=0)
     release_date = models.DateField()
 
