c8bacc510f0a10406550dd622bcf4815fa0622b8
Michael David <davidlearn27@gmail.com>
2023-04-01 08:40:53 +0100

    created  Comment model

--- core/models.py
+++ core/models.py
@@ -16,3 +16,16 @@ class Film(models.Model):
         ordering = ['clicks']
 
 
+class Comment(models.Model):
+    film = models.ForeignKey(Film, on_delete=models.CASCADE, related_name='comments')
+    comment = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comment
+
+    def save(self, *args, **kwargs):
+        film = Film.objects.get(id=self.film.id)
+        film.comment_count += 1
+        film.save()
+        return super().save(*args, **kwargs)
