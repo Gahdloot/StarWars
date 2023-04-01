7f8607f2df7dc613c5051197307155a086e26f7e
Michael David <davidlearn27@gmail.com>
2023-04-01 20:50:13 +0100

    added a Film Creation Serializer

--- core/serializer.py
+++ core/serializer.py
@@ -2,12 +2,17 @@ from rest_framework import serializers
 from .models import Film, Comment
 
 class FilmSerializer(serializers.ModelSerializer):
-    comment_count = serializers.SerializerMethodField()
+
 
     class Meta:
         model = Film
-        fields = ('id', 'title', 'release_date', 'comment_count')
+        fields = ('id', 'title', 'episode_id', 'release_date', 'comment_count')
 
+class FilmCreationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Film
+        fields = ('title', 'episode_id', 'release_date',)
 
 
 class CommentSerializer(serializers.ModelSerializer):
