829e15ca6cd03aecc7e4eb9232a4090c4e60aafc
Michael David <davidlearn27@gmail.com>
2023-04-02 08:36:55 +0100

    added redis url to env

--- starwars/settings.py
+++ starwars/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379',
+        'LOCATION': os.environ.get('REDIS_URL'),
     }
 }
 
