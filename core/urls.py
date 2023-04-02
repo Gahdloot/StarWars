ab312bd0ca4edc6a3a467c2381ad847c04be4496
Michael David <davidlearn27@gmail.com>
2023-04-02 06:43:14 +0100

    Added comment to file

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import FilmList
+from .views import FilmList, FilmComment
 urlpatterns = [
-    path('film-list/', FilmList.as_view())
+    path('film-list/', FilmList.as_view()),
+    path('comment/<int:id>', FilmComment.as_view())
     ]
